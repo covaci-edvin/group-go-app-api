38ef1b991cf565216e54639a18f6e1cb87509a71
Covaci Edvin <edvincov28@gmail.com>
2023-02-15 16:29:43 +0200

    added config.env file

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ process.on('uncaughtException', (err) => {
 
   process.exit(1);
 });
-
 dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
