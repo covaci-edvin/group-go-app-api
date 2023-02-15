0726635bfd21c2c6e0608dad7ac94fc3bf3a7acb
Covaci Edvin <edvincov28@gmail.com>
2023-02-15 17:39:03 +0200

    added config.env file

--- utils/email.js
+++ utils/email.js
@@ -8,7 +8,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Edvin Covai <${process.env.EMAIL_FROM}>`;
+    this.from = `Edvin Covaci <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
