4afee167092a27c9b022edd3ca550f6020e344ad
Covaci Edvin <edvincov28@gmail.com>
2023-05-08 14:33:08 +0300

    send email with sendgrid implemented

--- utils/email.js
+++ utils/email.js
@@ -2,6 +2,7 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
+const sgMail = require('@sendgrid/mail');
 
 module.exports = class Email {
   constructor(user, url, groupName = '', emailTo = '') {
@@ -19,8 +20,23 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
-      return 1;
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: 'test@example.com', // Change to your recipient
+        from: 'test@example.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent');
+        })
+        .catch((error) => {
+          console.log('❌');
+          console.error(error);
+        });
     }
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
